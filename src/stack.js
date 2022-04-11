103f1159c20a34c85626302f9028f2a60b111c09
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-04-11 21:38:22 +0300

    feat: realized stack

--- src/stack.js
+++ src/stack.js
@@ -13,17 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #items;
+  constructor() {
+    this.#items = [];
+  }
+  push(element) {
+    this.#items.push(element);
+    return this;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#items.pop();
   }
 
   peek() {
+    return this.#items[this.#items.length - 1]
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
